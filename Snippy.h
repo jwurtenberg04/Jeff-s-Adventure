6c3550f87f977f22437e82ceac2b6e469a30fc21
jwurtenberg04 <97301254+jwurtenberg04@users.noreply.github.com>
2023-11-08 10:37:31 -0500

    Update Snippy.h

--- Snippy.h
+++ Snippy.h
@@ -1,24 +1,39 @@
 #pragma once
-#include <filesystem>
 #include <SFML/Graphics.hpp>
 
 class Snippy
 {
 public:
-	int generate(std::filesystem::path asset_dir);
-	void walk_right(sf::RenderWindow &window, float direction);
-	void walk_left(sf::RenderWindow &window, float direction);
-	void draw(sf::RenderWindow &window, sf::View &view, int &switch_control);
+	int generate();
+	void walk_right(sf::RenderWindow &window);
+	void walk_left(sf::RenderWindow &window);
+	void draw(sf::RenderWindow &window, sf::View &view, int switch_control);
 	void spawn(int xPos, int yPos);
 	sf::FloatRect global_bounds();
-	void update_y();
-	float pos_x = 250.0f;
-	float pos_y = 395.0f;
-	float velocity_y = 0.0f;
-	static constexpr float walk_speed = 4.5f;
-	float snippy_jump_strength = 17.0f;
+	float pos_x = 600.0f;
+	float pos_y = 490.0f;
+	static constexpr float walk_speed = 2.5f;
 	sf::Sprite sprite;
 private:
+	int animation_index = 0;
+
+	static constexpr const char* snippyRight[] = {
+		"Snippy/Snippy-Right00.png", "Snippy/Snippy-Right00.png", "Snippy/Snippy-Right00.png", "Snippy/Snippy-Right01.png", "Snippy/Snippy-Right02.png", "Snippy/Snippy-Right03.png",
+		"Snippy/Snippy-Right04.png", "Snippy/Snippy-Right04.png", "Snippy/Snippy-Right04.png", "Snippy/Snippy-Right05.png", "Snippy/Snippy-Right06.png", "Snippy/Snippy-Right07.png",
+		"Snippy/Snippy-Right08.png", "Snippy/Snippy-Right08.png", "Snippy/Snippy-Right08.png", "Snippy/Snippy-Right09.png", "Snippy/Snippy-Right10.png", "Snippy/Snippy-Right11.png",
+		"Snippy/Snippy-Right12.png", "Snippy/Snippy-Right12.png", "Snippy/Snippy-Right12.png", "Snippy/Snippy-Right13.png", "Snippy/Snippy-Right14.png", "Snippy/Snippy-Right15.png",
+		"Snippy/Snippy-Right16.png", "Snippy/Snippy-Right16.png", "Snippy/Snippy-Right16.png", "Snippy/Snippy-Right17.png", "Snippy/Snippy-Right18.png", "Snippy/Snippy-Right19.png",
+		"Snippy/Snippy-Right20.png", "Snippy/Snippy-Right20.png", "Snippy/Snippy-Right20.png", "Snippy/Snippy-Right21.png", "Snippy/Snippy-Right22.png",
+	};
+	static constexpr const char* snippyLeft[] = {
+		"Snippy/Snippy-Left00.png", "Snippy/Snippy-Left00.png", "Snippy/Snippy-Left00.png", "Snippy/Snippy-Left01.png", "Snippy/Snippy-Left02.png", "Snippy/Snippy-Left03.png",
+		"Snippy/Snippy-Left04.png", "Snippy/Snippy-Left04.png", "Snippy/Snippy-Left04.png", "Snippy/Snippy-Left05.png", "Snippy/Snippy-Left06.png", "Snippy/Snippy-Left07.png",
+		"Snippy/Snippy-Left08.png", "Snippy/Snippy-Left08.png", "Snippy/Snippy-Left08.png", "Snippy/Snippy-Left09.png", "Snippy/Snippy-Left10.png", "Snippy/Snippy-Left11.png",
+		"Snippy/Snippy-Left12.png", "Snippy/Snippy-Left12.png", "Snippy/Snippy-Left12.png", "Snippy/Snippy-Left13.png", "Snippy/Snippy-Left14.png", "Snippy/Snippy-Left15.png",
+		"Snippy/Snippy-Left16.png", "Snippy/Snippy-Left16.png", "Snippy/Snippy-Left16.png", "Snippy/Snippy-Left17.png", "Snippy/Snippy-Left18.png", "Snippy/Snippy-Left19.png",
+	};
 	sf::Texture snippyIdleRight;
+	sf::Texture snippyRightTextures[std::size(snippyRight)];
+	sf::Texture snippyLeftTextures[std::size(snippyLeft)];
 };
 
