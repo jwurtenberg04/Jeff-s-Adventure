3015f4d3fb4ebea25b1efbe9cb67d4eae29db702
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2023-11-13 09:46:25 -0500

    Define missing variables

--- Source.cpp
+++ Source.cpp
@@ -45,6 +45,8 @@ int main() {
 	Snippy snippy;
 	Jeff jeff;
 	Game game;
+	int snippy_switch_control = 2;
+	bool snippy_alive = true;
 
 	// Load all of the images and convert them to textures once.
 	if (jeff.generate(asset_dir) == EXIT_FAILURE) {
