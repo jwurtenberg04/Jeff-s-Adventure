db5c25bc6b8ea5e0c6a00c7dd7b92423a57ec9cd
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2023-11-20 23:02:58 -0500

    Mark `collide_sprite` as static

--- Game.h
+++ Game.h
@@ -6,7 +6,7 @@ class Game
 {
 public:
 	int generate(std::filesystem::path asset_dir);
-	bool collide_sprite(sf::Sprite a, sf::Sprite b);
+	static bool collide_sprite(sf::Sprite a, sf::Sprite b);
 	bool collide_shape(sf::Sprite a, sf::ConvexShape b);
 	bool collide_rect_and_shape(sf::FloatRect rect, sf::ConvexShape shape);
 	void draw(sf::RenderWindow &window);
