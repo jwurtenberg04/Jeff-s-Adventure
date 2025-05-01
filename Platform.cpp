9bd83f946dd2e68dd5bd799199ffe08c5bc7d66d
jwurtenberg04 <joshuawurtenberg1@gmail.com>
2025-05-01 15:26:36 -0400

    Finished Migrating the Platform Class

--- Platform.cpp
+++ Platform.cpp
@@ -18,16 +18,16 @@ void Platform::draw(sf::RenderWindow& window) const {
 }
 
 std::optional<Collision> Platform::collide(const sf::FloatRect& other) const {
-	sf::FloatRect overlap;
-	if (!rectangle.getGlobalBounds().intersects(other, overlap)) {
+	std::optional<sf::FloatRect> overlap = rectangle.getGlobalBounds().findIntersection(other);
+	if (!overlap) {
 		// No overlap:
 		return std::nullopt;
 	}
 	// Just use position to guess the direction of the collision.
 	const auto center = rectangle.getPosition() + rectangle.getSize() / 2.0f;
-	const auto other_center = other.getPosition() + other.getSize() / 2.0f;
+	const auto other_center = other.position + other.size / 2.0f;
 	Direction direction;
-	if (overlap.width < overlap.height) {
+	if (overlap->size.x < overlap->size.y) {
 		// If there is less horizontal overlap than vertical overlap,
 		// consider this collision to be horizontal.
 		bool from_left = other_center.x < center.x;
@@ -37,5 +37,5 @@ std::optional<Collision> Platform::collide(const sf::FloatRect& other) const {
 		bool from_top = other_center.y < center.y;
 		direction = from_top ? Direction::top : Direction::bottom;
 	}
-	return Collision{ overlap, direction };
+	return Collision{ *overlap, direction };
 }
