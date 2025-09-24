aa94f5b2ce076a9865f8119f20c8f5029c09bce5
jwurtenberg04 <joshuawurtenberg1@gmail.com>
2025-09-24 14:35:23 -0400

    started working on eraser collisions

--- Eraser.cpp
+++ Eraser.cpp
@@ -13,8 +13,25 @@ void Eraser::move(sf::Time dt) {
 	position += velocity * dt.asSeconds();
 }
 
+void Eraser::draw_debug(sf::RenderWindow &window) {
+	sf::Sprite sprite {texture};
+	auto bounds = sprite.getGlobalBounds();
+	sf::RectangleShape shape { bounds.size };
+	shape.setPosition(bounds.position);
+	shape.setOutlineThickness(4.0f);
+	shape.setOutlineColor(sf::Color::Magenta);
+	shape.setFillColor(sf::Color::Transparent);
+	window.draw(shape);
+}
+
+sf::FloatRect Eraser::getRect() {
+	sf::Sprite sprite { texture };
+	return sprite.getGlobalBounds();
+}
 sf::Texture Eraser::texture{};
 
 bool Eraser::load_texture(const std::filesystem::path& asset_dir) {
 	return texture.loadFromFile(asset_dir / "eraser.png");
+
+	
 }
